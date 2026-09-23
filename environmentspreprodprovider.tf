07eaba41639365a2fb6ce927f44fd6e5023c6217
Amit Tiwari <er.amit.tiwari2011@gmail.com>
2026-09-23 20:46:53 +0530

    Add files via upload

---
+++ environmentspreprodprovider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.80.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-micro-dev"
+    storage_account_name = "netfliixstoraage"
+    container_name       = "tfstate"
+    key                  = "preprod.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
